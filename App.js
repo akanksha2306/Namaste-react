b259029d97ccc9d1b3e3493e42b308f087dfc3c8
akanksha2306 <kanjoliaakanksha@gmail.com>
2023-05-19 23:53:13 +0530

    episode1

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
  
-const parent = React.createElement("div",{id:"parent"},[
+const paren = React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
         React.createElement("h1", {}, "I am an h1 tag"),
         React.createElement("h2", {}, "I am an h2 tag"),
